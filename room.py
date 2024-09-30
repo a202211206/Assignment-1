a48c5ac986fdd4860a2145b06adef13b5861f073
a202211206 <albatoolalnuaimi28@gmail.com>
2024-09-30 18:39:19 +0400

    Update room.py

--- room.py
+++ room.py
@@ -1,8 +1,16 @@
 class Room:
-def __init__(self, room_type, room_number, price_per_night):
-self.room_type = room_type self.room_number = room_number self.price_per_night = price_per_night self.availability = True
-def check_availability(self): return self.availability
-def reserve_room(self): if self.availability:
-self.availability = False
-print(f"Room {self.room_number} reserved.") else:
-print(f"Room {self.room_number} is not available.")
+    def __init__(self, room_type, room_number, price_per_night):
+        self.room_type = room_type
+        self.room_number = room_number
+        self.price_per_night = price_per_night
+        self.availability = True
+
+    def check_availability(self):
+        return self.availability
+
+    def reserve_room(self):
+        if self.availability:
+            self.availability = False
+            print(f"Room {self.room_number} reserved.")
+        else:
+            print(f"Room {self.room_number} is not available.")
