baa4e0dc19ee22db2db545603a7a56b5c5c98ec0
a202211206 <albatoolalnuaimi28@gmail.com>
2024-09-30 18:41:49 +0400

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,9 +1,14 @@
 class Payment:
-def __init__(self, payment_id, amount, payment_type):
-self.payment_id = payment_id self.amount = amount self.payment_type = payment_type self.payment_status = "Pending"
-def process_payment(self):
-self.payment_status = "Completed"
-print(f"Payment {self.payment_id} of {self.amount} has been processed.")
-def refund_payment(self):
-self.payment_status = "Refunded"
-print(f"Payment {self.payment_id} has been refunded.")
+    def __init__(self, payment_id, amount, payment_type):
+        self.payment_id = payment_id
+        self.amount = amount
+        self.payment_type = payment_type
+        self.payment_status = "Pending"
+
+    def process_payment(self):
+        self.payment_status = "Completed"
+        print(f"Payment {self.payment_id} of {self.amount} has been processed.")
+
+    def refund_payment(self):
+        self.payment_status = "Refunded"
+        print(f"Payment {self.payment_id} has been refunded.")
