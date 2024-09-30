29cc2c80c45b3d72c222660c58da122f331c6f7e
a202211206 <albatoolalnuaimi28@gmail.com>
2024-09-30 18:31:02 +0400

    Update guest.py

--- guest.py
+++ guest.py
@@ -1,1 +1,8 @@
-
+class Guest:
+def __init__(self, guest_name, email, phone, payment_info):
+self.guest_name = guest_name self.email = email
+self.phone = phone self.payment_info = payment_info
+def view_reservation(self, reservation):
+print(f"Viewing reservation for {self.guest_name}: {reservation.reservation_id}, Status: {reservation.status}")
+def make_payment(self, amount):
+print(f"Payment of {amount} made by {self.guest_name}.")
